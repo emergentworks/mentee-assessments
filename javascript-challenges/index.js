a87f9e6170182d62c6fbe909f2fb82b54cda64ea
vera protopopova <vera@codeclimate.com>
2021-10-27 20:28:32 -0400

    exampple challenges

--- javascript-challenges/index.js
+++ javascript-challenges/index.js
@@ -1,5 +1,22 @@
-let challenge = () => {
-  console.log("hello world")
-  // all the mentee's code will go in here?
+class Challenge {
+
+  // ****************************************************
+  // START CODING HERE!
+  // ****************************************************
+
+  challenge = () => {
+    console.log("hello world")
+    // all the mentee's code will go in here?
+  }
+
+  // update function add() to take in two parameters and return their sum
+  add = (a, b) => {
+  // *****hint look up javascript function parameters*****
+  }
+
+
+  // *******************************************************
 }
-module.exports = challenge;
+
+
+module.exports = new Challenge();
--- javascript-challenges/test.js
+++ javascript-challenges/test.js
@@ -1,6 +1,10 @@
 
-const challenge = require('./index');
+const Challenge = require('./index');
 
-test('challenge is a function', () => {
-  expect(typeof challenge).toEqual(`function`);
+test('#challenge is a function', () => {
+  expect(typeof Challenge.challenge).toEqual(`function`);
+});
+
+test('#add takes in two arguments and returns their sum', () => {
+  expect(Challenge.add(5, 5)).toEqual(10);
 });
