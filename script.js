aa69707340effcec760671510da4b31da7c3df70
Garvey Moore <89597268+Cheluchi@users.noreply.github.com>
2021-12-01 08:25:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ let sum = (a,b) => {
   return parseInt(a) + parseInt(b);
 }
 
-let counter = () => {
+let counter = () => {package IncludeHelp;
+
+public class Print_1_To_10_UsingFor 
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
 }
 
 let oddCounter = () => {
